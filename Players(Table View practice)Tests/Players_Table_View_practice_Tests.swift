ce35cf4364344bf212614b3ee48dd5b894e70326
Franco Pecci <fapeccisan@gmail.com>
2023-08-21 09:01:28 -0400

    Initial Commit

---
+++ Players(Table View practice)Tests/Players_Table_View_practice_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Players_Table_View_practice_Tests.swift
+//  Players(Table View practice)Tests
+//
+//  Created by Franco Pecci on 2023-08-21.
+//
+
+import XCTest
+@testable import Players_Table_View_practice_
+
+class Players_Table_View_practice_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
